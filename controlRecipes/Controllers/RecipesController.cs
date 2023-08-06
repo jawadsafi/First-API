8c47c315ec96c25167a4c71faae2345fe840850e
ilyassafi <jsafi4428@gmail.com>
2023-08-06 11:53:04 -0400

    Adding my favorite dishes

---
+++ controlRecipes/Controllers/RecipesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace controlRecipes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetDishes()
+        {
+            string[] dishes = { "Oxtail", "CurryChicken", "Dumplings" };
+            return dishes;
+        }
+    }
+}
